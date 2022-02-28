7a175da566c036e7b42b1d1993a57bffdbe708cc
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-02-28 08:57:28 +0500

    Bismillah.... start project

---
+++ src/main/java/uz/pdp/task11_1/Task111Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.task11_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task111Application.class, args);
+    }
+
+}
